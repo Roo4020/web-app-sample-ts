4b52d662d42cc4d1acacd0f860c1655eb0ab71f0
sinowin <sinowin4020@gamil.com>
2020-12-14 22:04:59 +0900

    firebaseのインストール方法修正

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXLvCgy05BatUcqu3fWerKFsfgbCOJEUo",
--- src/store/canvas/actions.ts
+++ src/store/canvas/actions.ts
@@ -2,13 +2,14 @@ import { ActionTree } from "vuex";
 import { IcanvasState } from "./models";
 import { RootState } from "../RootState";
 
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 export const actions: ActionTree<IcanvasState, RootState> = {
   getCanvas(): void {
-    firebase.firestore().collection('canvas').get().then((doc: any) => {
-      doc.forEach((element: any) => {
-        // console.log(element.data());
+    firebase.firestore().collection('canvas').get().then((snapShot: any) => {
+      snapShot.forEach((doc: any) => {
+        console.log(doc.data());
       });
     }).catch((error: any) => {
       console.log(error);
