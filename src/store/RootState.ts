058ec9a0aa1be6629d951d2031246631f95f0982
sinowin <sinowin4020@gamil.com>
2021-01-08 17:45:32 +0900

    canvasとmodalの文を追加

--- src/store/RootState.ts
+++ src/store/RootState.ts
@@ -1,7 +1,11 @@
 import { IauthState } from "./auth/models";
+import { IcanvasState } from "./canvas/models";
 import { IcommonState } from "./common/models";
+import { ImodalState } from "./modal/models";
 
 export interface RootState {
     auth: IauthState;
+    canvas: IcanvasState;
     common: IcommonState;
+    modal: ImodalState;
 }
